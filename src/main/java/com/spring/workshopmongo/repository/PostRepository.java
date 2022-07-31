42c963bf7fefd3a41032e6f84e83ec87b96f5164
João Paulo <carrijojp@gmail.com>
2022-07-30 21:33:44 -0300

    Consulta simples com @Query

--- src/main/java/com/spring/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/spring/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.spring.workshopmongo.repository;
 import com.spring.workshopmongo.domain.Post;
 import com.spring.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {  // necessario dois dados para o MongoRepository
                                                                          // saber o tipo de dominio que irá referenciar
-  List<Post> findByTitleContainingIgnoreCase(String text);   // ignorar maiuscula e minusc
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")  // parametro na url
+  List<Post> searchTitle(String text);
+  List<Post> findByTitleContainingIgnoreCase(String text);   // ignorar maiuscula e minusculo
 
 }
